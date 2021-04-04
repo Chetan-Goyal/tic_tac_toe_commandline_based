411d8339a827aed501057cebdf34c5eed1478e71
Chetan Goyal <55137330+Chetan-Goyal@users.noreply.github.com>
2021-04-05 01:26:32 +0530

    Minor Improvements

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 nor = 3    # number of rows
 noc = 3    # number of columns
-l = [ [None,None,None], [None,None,None], [None,None,None]  ]  # list storing the data
-turn = [False, False]  # storing turns of the players based on index
-symbol = [None,None]   # storing symbol choosed by players based on index
+l = [ [None,None,None], [None,None,None], [None,None,None]  ]  # list storing the table data
+turn = [False, False]  # storing turn of the players
+symbol = [None,None]   # storing symbol chosen by players
